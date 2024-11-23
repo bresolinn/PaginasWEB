0868290c6dc494cea146d8ba547fe9d37706694a
bresolinn <gabriel.bresolin13@gmail.com>
2024-11-22 22:42:14 -0300

    Update script.js

--- casar/script.js
+++ casar/script.js
@@ -1,27 +1,64 @@
-document.querySelector('#nao').addEventListener('mouseover', function() {
-    const titulo = document.querySelector('h1');
-    const botao = document.querySelector('#nao');
-    const larguraBotao = botao.offsetWidth;
-    const alturaBotao = botao.offsetHeight;
-    const alturaTitulo = titulo.offsetHeight;
+let g = 0;
+const botaoNao = document.querySelector('#nao');
+const musica = document.getElementById('musica');
 
-    const maxAltura = window.innerHeight - alturaBotao;
-    const numeroAleatorio1 = alturaTitulo * 3.5 + Math.floor(Math.random() * (maxAltura - alturaTitulo * 4));
+const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
-    const maxLargura = window.innerWidth - larguraBotao;
-    const numeroAleatorio2 = Math.floor(Math.random() * (maxLargura - alturaTitulo - alturaBotao + 1));
+if (!isMobile) {
+    botaoNao.addEventListener('mouseover', function() {
+        const titulo = document.querySelector('h1');
+        const botao = document.querySelector('#nao');
+        const larguraBotao = botao.offsetWidth;
+        const alturaBotao = botao.offsetHeight;
+        const alturaTitulo = titulo.offsetHeight;
 
-    botao.style.position = 'absolute';
-    botao.style.top = `${numeroAleatorio1}px`;
-    botao.style.left = `${numeroAleatorio2}px`;
-});
+        const maxAltura = window.innerHeight - alturaBotao;
+        const numeroAleatorio1 = alturaTitulo * 3.5 + Math.floor(Math.random() * (maxAltura - alturaTitulo * 4));
+
+        const maxLargura = window.innerWidth - larguraBotao;
+        const numeroAleatorio2 = Math.floor(Math.random() * (maxLargura - alturaTitulo - alturaBotao + 1));
+
+        botao.style.position = 'absolute';
+        botao.style.top = `${numeroAleatorio1}px`;
+        botao.style.left = `${numeroAleatorio2}px`;
+
+        g += 1;
+
+        if (g % 5 === 0) {
+            window.alert('pq vc não quer?? desamou');
+        }
+    });
+} else {
+    botaoNao.addEventListener('click', function() {
+        const titulo = document.querySelector('h1');
+        const botao = document.querySelector('#nao');
+        const larguraBotao = botao.offsetWidth;
+        const alturaBotao = botao.offsetHeight;
+        const alturaTitulo = titulo.offsetHeight;
+
+        const maxAltura = window.innerHeight - alturaBotao;
+        const numeroAleatorio1 = alturaTitulo * 3.5 + Math.floor(Math.random() * (maxAltura - alturaTitulo * 4));
+
+        const maxLargura = window.innerWidth - larguraBotao;
+        const numeroAleatorio2 = Math.floor(Math.random() * (maxLargura - alturaTitulo - alturaBotao + 1));
+
+        botao.style.position = 'absolute';
+        botao.style.top = `${numeroAleatorio1}px`;
+        botao.style.left = `${numeroAleatorio2}px`;
+
+        g += 1;
+
+        if (g % 5 === 0) {
+            window.alert('pq vc não quer?? desamou');
+        }
+    });
+}
 
 document.querySelector('#sim').addEventListener('click', function() {
     document.querySelector('#sim').style.display = 'none';
     document.querySelector('#nao').style.display = 'none';
     document.querySelector('.oculto').style.display = 'block';
-    var musica = document.getElementById('musica');
-        musica.play();
+    musica.play();
 });
 
 document.querySelector('#reiniciar').addEventListener('click', function() {
@@ -30,15 +67,5 @@ document.querySelector('#reiniciar').addEventListener('click', function() {
     document.querySelector('.oculto').style.display = 'none';
     document.querySelector('#nao').style.position = '';
     musica.pause();
-        musica.currentTime = 0;
-});
-
-let g = 0;
-
-document.querySelector('#nao').addEventListener('mouseover', function() {
-    g += 1;
-
-    if (g%5 === 0) {
-        window.alert('pq vc não quer?? desamou');
-    }
+    musica.currentTime = 0;
 });
